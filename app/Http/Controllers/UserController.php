456cbddb1ac55848603cbc5251514eb7c01f2307
Chuug <collot.hugo@gmail.com>
2020-10-25 21:20:41 +0100

    Commentaire

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,6 +25,7 @@ class UserController extends Controller
 
     public function updateProfile(Request $request)
     {
+        //Commentaire
         $this->authorize('updateProfile', User::class);
 
         $rules = [
